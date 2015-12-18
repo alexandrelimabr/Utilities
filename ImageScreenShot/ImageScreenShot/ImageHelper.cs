7923cf4d72e94764232522e3529d6c0506601e65
alexandrelimabr <alexandre.neo@gmail.com>
2015-12-18 09:23:39 -0200

    How to get a Screenshot and then crop that image

---
+++ ImageScreenShot/ImageScreenShot/ImageHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace WpfApplication1
+{
+    static class ImageHelper
+    {
+
+        public static Image CaptureScreen(int sourceX, int sourceY, int destX, int destY,
+            System.Drawing.Size regionSize)
+        {
+            Bitmap bmp = new Bitmap(regionSize.Width, regionSize.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(sourceX, sourceY, destX, destY, regionSize);
+
+            Bitmap result = cropAtRect(bmp, new Rectangle(sourceX, sourceY, regionSize.Width, regionSize.Height));
+
+            return result;
+        }
+
+        private static Bitmap cropAtRect(this Bitmap b, Rectangle r)
+        {
+            Bitmap nb = new Bitmap(r.Width, r.Height);
+            Graphics g = Graphics.FromImage(nb);
+            g.DrawImage(b, -r.X, -r.Y);
+            return nb;
+        }
+
+    }
+}
---
+++ ImageScreenShot/ImageScreenShot/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Windows;
+using System.IO;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string fileScreenShotFull = @"c:\temp\Print.jpg";
+        static string fileResized = @"c:\temp\PrintResized.jpg";
+        static int x;
+        static int y;
+
+        static int width;
+        static int height;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public object Screen { get; private set; }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //Faz um screenshot da tela inteira e salva como imagem
+            int totalWidth = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+            int totalHeight = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+
+            Image newImage = ImageHelper.CaptureScreen(0, 0, 0, 0, new System.Drawing.Size(totalWidth, totalHeight));
+            newImage.Save(fileScreenShotFull);
+            imgTarget.Source = new BitmapImage(new Uri(fileScreenShotFull));
+
+            //Pega as coordenadas do controle original onde a imagem é mostrada
+            System.Windows.Point p = imgSource.PointToScreen(new System.Windows.Point(0d, 0d));
+
+            x = (int)p.X;
+            y = (int)p.Y;
+
+            width = (int)imgSource.Width - 1;
+            height = (int)imgSource.Height;
+
+            //Faz um crop da imagem criada acima usando as dimensões do controle original e salva esta nova imagem
+            Rectangle cloneRect = new Rectangle(x, y, width, height);
+            Bitmap bmpImage = new Bitmap(fileScreenShotFull);
+            Bitmap clonedBitMap = bmpImage.Clone(cloneRect, bmpImage.PixelFormat);
+            clonedBitMap.Save(fileResized);
+            clonedBitMap.Dispose();
+            imgFinal.Source = new BitmapImage(new Uri(fileResized));
+
+        }
+
+    }
+}
